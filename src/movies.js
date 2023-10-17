29ee2e36315ecdd1de69bf5bd5d8eec6bc5ae63a
Omayma El Hadari <om.elhadari@gmail.com>
2023-10-17 18:36:34 +0200

    solved

--- src/movies.js
+++ src/movies.js
@@ -8,7 +8,7 @@ should return a new array with the same length as the original one
 */
 
 function getAllDirectors(moviesArray) {
-return moviesArray.map(movie=> movie.director)
+return moviesArray.map(movie => movie.director)
 }
 
 
@@ -55,7 +55,7 @@ function scoresAverage(moviesArray) {
         }
     }, 0); 
     
-    let averageScore = scores/moviesArray.length;
+    let averageScore = scores / moviesArray.length;
 
     return Number(averageScore.toFixed(2));
 
@@ -75,13 +75,13 @@ should return 0 if there is no Drama movie
 
 function dramaMoviesScore(moviesArray) {
 
-    let dramaMovies = moviesArray.filter(movie => movie.genre.includes('Drama'));
+    const dramaMovies = moviesArray.filter(movie => movie.genre.includes('Drama'));
 
-    if (dramaMovies.length=== 0){
+   /* if (dramaMovies.length === 0){
         return 0;
     }
 
-    let overallScore = dramaMovies.reduce((totalScore, film)=>{
+    let overallScore = dramaMovies.reduce((totalScore, film) => {
         if (film.score === undefined){
             return totalScore
         }else{
@@ -91,18 +91,80 @@ function dramaMoviesScore(moviesArray) {
 
     const avg = overallScore / dramaMovies.length;
 
-    return Number(avg.toFixed(2));
+    return Number(avg.toFixed(2)); */
+
+    return scoresAverage(dramaMovies);
+
 }
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+
+/* should return an array
+should return a new array, not mutate the original one
+should return the element in a single element array
+should return the new array in ascending order
+should order movies with the same year by their title, alphabetically */
+
+function orderByYear(moviesArray) {
+
+    const chronologicalOrder = [...moviesArray.sort((movie1,movie2)=>{  
+        if (movie1.year < movie2.year){
+            return -1;
+        }else if (movie1.year > movie2.year){
+            return 1
+        }else{
+            return movie1.title.localeCompare(movie2.title);
+        }
+        
+    })]
+
+    return chronologicalOrder;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+
+/* should return an array
+should return a new array, not mutate the original one
+should only return the title of the movies, each value should be a string
+should return all of items when the array passed has fewer than 20 items
+If there are more than 20 elements, return only 20 of them.
+should order them alphabetically.
+should return the top 20 after ordering them alphabetically.
+*/
+
+
+function orderAlphabetically(moviesArray) {
+
+    const movies = [...moviesArray];
+    const moviesTitles= movies.map(film => film.title);
+
+    const orderedMovies = moviesTitles.sort();
+
+    // .sort() va a ordenar usando su método por defecto, que en este caso nos sirve
+
+
+    return orderedMovies.slice(0,20)
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+
+/* should return an array
+should return a new array, not mutate the original one
+should return an array of movies with duration as a number
+should return an array of movies with the correct duration for a 31 minute movie
+should return an array of movies with the correct duration for a 341 minute movie
+should return an array of movies with the correct duration for a 2 hour movie
+*/
+// https://www.freecodecamp.org/news/how-to-convert-a-string-to-a-number-in-javascript/
+function turnHoursToMinutes(moviesArray) {
+    
+
+
+
+
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
